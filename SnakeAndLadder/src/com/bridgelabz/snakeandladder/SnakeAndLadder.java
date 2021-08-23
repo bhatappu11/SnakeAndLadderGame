7413463180e00d76cd89dc8dc3b9675ab7a261b2
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-23 08:27:40 +0530

    ADDED: initialised player position to zero

--- SnakeAndLadder/src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ SnakeAndLadder/src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -4,7 +4,8 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game");
-
+		System.out.println("The Player Starts at Position 0");
+		int currentPosition1=0;
 	}
 
 }

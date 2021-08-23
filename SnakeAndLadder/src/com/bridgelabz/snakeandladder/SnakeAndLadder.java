006cbed2371c52af9a7a54af8e0ca1ed8f154d31
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-23 08:38:27 +0530

    ADDED: Options for no play,snake and ladder

--- SnakeAndLadder/src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ SnakeAndLadder/src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,13 +1,29 @@
 package com.bridgelabz.snakeandladder;
 
 public class SnakeAndLadder {
+		public static final int NO_PLAY=0;
+		public static final int LADDER=1;
+		public static final int SNAKE=2;
+		public static void main(String[] args) {
+			System.out.println("Welcome to snake and ladder game");
+			System.out.println("The Player Starts at Position 0");
+			int currentPosition1=0,numberOnDice=0;
+			numberOnDice=(int)((Math.random()*100)%6)+1;
+			System.out.println("Number on dice "+numberOnDice);
+			int option=(int)(Math.random()*100)%3;
+			switch(option) {
+			case NO_PLAY:
+			System.out.println("Option is No Play");
+			break;
+			case LADDER:
+			System.out.println("Option is Ladder");
+			currentPosition1=currentPosition1+numberOnDice;
+			break;
+			case SNAKE:
+			System.out.println("Option is Snake");
+			currentPosition1=currentPosition1-numberOnDice;
+			break;
+		}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to snake and ladder game");
-		System.out.println("The Player Starts at Position 0");
-		int currentPosition1=0,numberOnDice=0;
-		numberOnDice=(int)((Math.random()*100)%6)+1;
-		System.out.println("Number on dice "+numberOnDice);
-	}
-
+}
 }
